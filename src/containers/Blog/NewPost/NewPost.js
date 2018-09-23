8dd084348f6263e4c90e6e79dee1cee1bd0f0d97
Aditya Singh <adityasingh222247@gmail.com>
2018-09-23 17:31:14 +0530

    Routing initial commit

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 
 import './NewPost.css';
@@ -9,6 +9,10 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     };
+
+    componentDidMount() {
+        console.log(this.props)
+    }
 
     postDataHandler = () => {
         const data = {
@@ -28,12 +32,12 @@ class NewPost extends Component {
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title}
-                       onChange={(event) => this.setState({title: event.target.value})}/>
+                    onChange={(event) => this.setState({ title: event.target.value })} />
                 <label>Content</label>
                 <textarea rows="4" value={this.state.content}
-                          onChange={(event) => this.setState({content: event.target.value})}/>
+                    onChange={(event) => this.setState({ content: event.target.value })} />
                 <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+                <select value={this.state.author} onChange={(event) => this.setState({ author: event.target.value })}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
